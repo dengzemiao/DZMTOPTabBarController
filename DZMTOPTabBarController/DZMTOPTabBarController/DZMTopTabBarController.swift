4d143bf2df492dca82c40d2e610dc489ac90223e
dengzemiao <dengzemiaovip@163.com>
2017-05-08 14:36:09 +0800

    。。

--- DZMTOPTabBarController/DZMTOPTabBarController/DZMTopTabBarController.swift
+++ DZMTOPTabBarController/DZMTOPTabBarController/DZMTopTabBarController.swift
@@ -284,34 +284,6 @@ class DZMTopTabBarController: UIViewController,DZMTopBarDelegate,DZMCycleScrollV
         currentController = controller
     }
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        
-//        super.viewWillAppear(animated)
-//        
-//        currentController?.viewWillAppear(animated)
-//    }
-//    
-//    override func viewDidAppear(_ animated: Bool) {
-//        
-//        super.viewDidAppear(animated)
-//        
-//        currentController?.viewDidAppear(animated)
-//    }
-//    
-//    override func viewWillDisappear(_ animated: Bool) {
-//        
-//        super.viewWillDisappear(animated)
-//        
-//        currentController?.viewWillDisappear(animated)
-//    }
-//    
-//    override func viewDidDisappear(_ animated: Bool) {
-//        
-//        super.viewDidDisappear(animated)
-//        
-//        currentController?.viewDidDisappear(animated)
-//    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
